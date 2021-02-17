13e1a92f8d8c2edd70e4c7f2ff39ad97df41f093
MengzhuLi821 <limz2018@163.com>
2021-02-17 15:46:38 +0800

    add image

--- major_page.py
+++ major_page.py
@@ -12,6 +12,10 @@ from tkinter import ttk
 import calendar
 from tkinter import colorchooser as cc
 import oscilloscope as oscill
+
+from tkinter import *
+from PIL import ImageTk,Image
+
 import serial
 import time
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
@@ -21,9 +25,11 @@ from matplotlib import style
 
 global bgc
 bgc = "black"
+global canvas
+global img_gif_1
 
 def create(name,login):
-    main_page = tk.Tk()
+    #main_page = tk.Tk()
     date = calendar.datetime.datetime
 
     def enter_potentiometer():
@@ -40,9 +46,17 @@ def create(name,login):
         bgc = colour_name
         name_label['fg'] = bgc
 
+    def display_image():
+        global canvas
+        canvas.create_image(200, 200, image=img_gif_1)
+        #print("hiiii")
+        #img_label = tk.Label(main_page, text="Welcome to the Electrical Circuit Tutorials! Dear " + name)
+        #img_label.grid(row=4, column=1)
+
     login.destroy()
+    main_page = tk.Tk()
     main_page.title("Tutorial List")
-    main_page.geometry('512x400')
+    main_page.geometry('800x640')
 
     colour_but = tk.Button(main_page, text="Change colour", command=pick_colour)
     colour_but.grid(row=2, column=0)
@@ -50,14 +64,24 @@ def create(name,login):
     pot_but = tk.Button(main_page, text="Potentiometer", command=enter_potentiometer)
     pot_but.grid(row=3, column=0)
 
+    img_but = tk.Button(main_page, text="image", command=display_image)
+    img_but.grid(row=4, column=0)
+
     name_label = tk.Label(main_page, text = "Welcome to the Electrical Circuit Tutorials! Dear "+name)
     today = date.today()
     date_info = today.strftime("%d/%b/%Y %H:%M:%S")
     date_label = tk.Label(main_page, text="Today is "+date_info)
     name_label.grid(row=0, column=1)
     date_label.grid(row=1, column=1)
+    global canvas
+    global img_gif_1
+    canvas = Canvas(main_page, width=600, height=500)
+    canvas.grid(row=4, column=1)
+
+
+    img_gif_1 = ImageTk.PhotoImage(Image.open("D:/Peach__Happer/ECE大三/hackathon/1.png"))
 
     main_page.mainloop()
 
-#root = tk.Tk()
-#create("Mengzhu", root)
+root = tk.Tk()
+create("Mengzhu", root)
